220de87299147e07de393765c4e2d24b02c3c227
Muskaan Parikh <muskaan.parikh@bignited.be>
2024-12-09 10:16:17 +0100

    Chore: push day 6 part 2 that doesn't work but Merlijn wanted to see

--- #6/code.py
+++ #6/code.py
@@ -6,6 +6,7 @@ amountOfColumns = len(CONTENT[0])
 
 labMap = []
 positionsOnMap = 0
+possibleObstaclesOnMap = 0
 
 for x in range(amountOfRows): 
     row = []
@@ -16,6 +17,8 @@ for x in range(amountOfRows):
 DIRECTIONS = ["N", "E", "S", "W"]
 directionTowards = "N"
 GUARD = "^"
+OBSTACLE = "#"
+movementsGuard = []
 
 def turnRight():
     indexCurrentDirection = DIRECTIONS.index(directionTowards) 
@@ -25,16 +28,16 @@ def turnRight():
 def checkForOTheEnd(currentX, currentY):
     match directionTowards:
         case "N": return currentX == 0
-        case "E": return currentX == amountOfRows-1 and currentY == amountOfColumns-2
+        case "E": return currentY == amountOfColumns-1
         case "S": return currentX == amountOfRows-1
         case "W": return currentY == 0
 
 def checkForObstacleAhead(currentX, currentY):
     match directionTowards:
-        case "N": return labMap[currentX-1][currentY] == "#"
-        case "E": return labMap[currentX][currentY+1] == "#"
-        case "S": return labMap[currentX+1][currentY] == "#"
-        case "W": return labMap[currentX][currentY-1] == "#"
+        case "N": return labMap[currentX-1][currentY] == OBSTACLE
+        case "E": return labMap[currentX][currentY+1] == OBSTACLE
+        case "S": return labMap[currentX+1][currentY] == OBSTACLE
+        case "W": return labMap[currentX][currentY-1] == OBSTACLE
 
 def walkStraight(currentX, currentY):
     match directionTowards:
@@ -48,16 +51,66 @@ def findGuard():
         if GUARD in labMap[x]: return [x, labMap[x].index(GUARD)]
 
 guard_x, guard_y = findGuard()
+movementsGuard.append([directionTowards, guard_x, guard_y])
 
 while checkForOTheEnd(guard_x, guard_y) == False:
     if checkForObstacleAhead(guard_x, guard_y) == False:
         guard_x, guard_y = walkStraight(guard_x, guard_y)
         labMap[guard_x][guard_y] = GUARD
+        movementsGuard.append([directionTowards, guard_x, guard_y])
     else:
         directionTowards = turnRight()
 
-for i in range(amountOfRows): 
+for i in range(amountOfRows):
     positionsOnMap = positionsOnMap + labMap[i].count(GUARD)
 
 print("#6.1 Guard Patrol steps: ", positionsOnMap)
-print("#6.2 Guard Patrol steps: ", 0)
+
+def resetMap():
+    for x in range(amountOfRows): 
+        row = []
+        for y in range(amountOfColumns-1):
+            row.append(CONTENT[x][y])    
+        labMap.append(row)
+
+visited = []
+resetMap()
+guard_x, guard_y = findGuard()
+directionTowards = "N"
+
+print(movementsGuard)
+
+for x in range(1, len(movementsGuard)-1):
+    shouldContinue = True
+    # directionTowards, guard_x, guard_y = movementsGuard[x]
+    
+    for y in range(1, x):
+        if (movementsGuard[y][1] == movementsGuard[x + 1][1] and movementsGuard[y][2] == movementsGuard[x + 1][2]):
+            shouldContinue = False 
+            break
+       
+    if shouldContinue == False:
+        continue
+               
+    labMap[movementsGuard[x+1][1]][movementsGuard[x+1][2]] = "#"
+    visited = []
+
+    while checkForOTheEnd(guard_x, guard_y) == False:
+        print("guard", directionTowards, guard_x, guard_y)
+        if [directionTowards, guard_x, guard_y] in visited:
+            directionTowards, guard_x, guard_y = movementsGuard[x]
+            possibleObstaclesOnMap += 1
+            break
+        
+        else:
+            if checkForObstacleAhead(guard_x, guard_y) == False:
+                guard_x, guard_y = walkStraight(guard_x, guard_y)
+                visited.append([directionTowards, guard_x, guard_y])
+                print(visited)
+            else:
+                directionTowards = turnRight()
+    resetMap()
+    
+        
+    
+print("#6.2 Guard Patrol steps: ", possibleObstaclesOnMap)
